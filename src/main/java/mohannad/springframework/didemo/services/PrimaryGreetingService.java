e11ea6b32d60bdd75abcd9f98b98b0a283ff8ce3
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-05-20 20:48:59 +0200

    creat factory beans for services

--- src/main/java/mohannad/springframework/didemo/services/PrimaryGreetingService.java
+++ src/main/java/mohannad/springframework/didemo/services/PrimaryGreetingService.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service
-@Profile({"en" , "default"})
-@Primary
+//@Service
+//@Profile({"en" , "default"})
+//@Primary
 public class PrimaryGreetingService implements GreetingService {
 
-    @Autowired
+//    @Autowired
     private GreetingRepository greetingRepository;
+
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository=greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
--- src/main/java/mohannad/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/mohannad/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service
-@Profile("es")
-@Primary
+//@Service
+//@Profile("es")
+//@Primary
 public class PrimarySpanishGreetingService implements GreetingService {
-    @Autowired
+//    @Autowired
     private GreetingRepository greetingRepository;
+
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
